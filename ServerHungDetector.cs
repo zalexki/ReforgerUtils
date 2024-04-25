b41167af82bcd9f76b2ff780544aae1065c4e20c
zalexki <a.haumaitre@cityscoot.eu>
2024-04-25 03:26:05 +0200

    fix

--- ServerHungDetector.cs
+++ ServerHungDetector.cs
@@ -22,7 +22,7 @@ public class ServerHungDetector : BackgroundService
     public ServerHungDetector(ILogger<ServerHungDetector> logger)
     {
         _logger = logger;
-        _timeout = TimeSpan.FromSeconds(200);
+        _timeout = TimeSpan.FromSeconds(250);
         _dockerClient = new DockerClientConfiguration().CreateClient();
     }
 
